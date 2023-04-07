fb3e39389768619bf43f51f1244f832edd4c867b
DESKTOP-B8DETJS\PC <william.sauve@decenternet.net>
2023-04-08 04:13:14 +0900

    [ADD] Added some buildings, a ground texture, and mesh combiner. [ADD] Creating UI for game scene. [ADD] Setting up some stuff for use when user needs to mouse of things.

---
+++ Assets/Scripts/Gameplay/PlayerControls.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GwentClone.Gameplay
+{
+    public class PlayerControls : MonoBehaviour
+    {
+
+        private void Update()
+        {
+            //Used for mousing over cards in the scene. 
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Do something with the hit object
+                Debug.Log("Hit object: " + hit.transform.name);
+            }
+        }
+    }
+}
