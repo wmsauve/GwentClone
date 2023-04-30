006213ecd4738ff1e4e0dd68615c238779df084f
DESKTOP-B8DETJS\PC <william.sauve@decenternet.net>
2023-05-01 02:47:35 +0900

    [ADD] Added the rest of the cards. [CHANGE] Changed the way scord works in the card data since there are some unit cards that scorch other than their own placement.

--- Assets/Scripts/Cards/Card.cs
+++ Assets/Scripts/Cards/Card.cs
@@ -9,18 +9,27 @@ namespace GwentClone
     [CreateAssetMenu(fileName = "Card", menuName = "ScriptableObjects/GwentCard")]
     public class Card : ScriptableObject
     {
+        [Header("Identifier Related")]
+        [Tooltip("Name of the card, but also used as identifier in code.")]
         public string id;
+        [Tooltip("Sprite of card.")]
+        public Sprite cardImage;
 
+        [Header("Card Information Related")]
         public EnumUnitType unitType;
         public EnumCardType cardType;
         public EnumUnitPlacement unitPlacement;
         public EnumFactionType factionType;
         public List<EnumCardEffects> cardEffects;
+
+        [Header("Interact With Mechanics Related")]
         public int cardPower;
         public int maxPerDeck;
+        [Tooltip("Used for mustering. Usually the same as ID. Can be different if a specific muster mechanic is utilized.")]
         public string musterTag;
-
-        public Sprite cardImage;
+        [Tooltip("Specify scorch target since cards can either target their placement or any placement for scorching.")]
+        public EnumUnitPlacement scorchTarget;
+        
     }
 
 }
